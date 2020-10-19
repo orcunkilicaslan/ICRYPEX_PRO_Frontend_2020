dbb9ff13508d0c34aeb6f24abe76cba5125f4a9a
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-10-19 20:42:07 +0300

    Detect Browser Added

---
+++ assets/src/script/plugin/detect-browser.js
@@ -0,0 +1,17 @@
+
+/* Detect Browser */
+const {detect} = require('detect-browser');
+const browser = detect();
+
+document.querySelector('html').classList.add(browser.name);
+
+if (browser) {
+    //console.log(browser.name);
+    //console.log(browser.version);
+    //console.log(browser.os);
+}
+
+if (browser.name === "chrome") {
+    //console.log("Bu tarayıcı chrome");
+}
+/* Detect Browser */
