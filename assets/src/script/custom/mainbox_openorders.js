89a2136a0b418cb023b7e5a69dd8c11b5868d91d
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-10-22 17:13:15 +0300

    Main Transaction History Tab Finished

---
+++ assets/src/script/custom/mainbox_openorders.js
@@ -0,0 +1,48 @@
+import PerfectScrollbar from 'perfect-scrollbar';
+
+/* Open Orders Tab Perfect Scrollbar */
+const OoTabOorArea      = document.querySelector('.openorders-taborders');
+const OoTabOorScrllHead = document.querySelector('.openorders-taborders-table .scrollbar-tbl-th');
+const OoTabOorScrllBody = document.querySelector('.openorders-taborders-table .scrollbar-tbl-tb');
+const OoTabHstArea      = document.querySelector('.openorders-tabhistory');
+const OoTabHstScrllTble = document.querySelector('.openorders-tabhistory-table .scrollbar-horizontal');
+const OoTabHstScrllHead = document.querySelector('.openorders-tabhistory-table .scrollbar-tbl-th');
+const OoTabHstScrllBody = document.querySelector('.openorders-tabhistory-table .scrollbar-tbl-tb');
+
+const psOoTabOOr        = new PerfectScrollbar(OoTabOorScrllBody, {wheelSpeed: 0.50, wheelPropagation: true, minScrollbarLength: 10, suppressScrollX: true});
+const psOoTabHTb        = new PerfectScrollbar(OoTabHstScrllTble, {wheelSpeed: 0.50, wheelPropagation: true, minScrollbarLength: 10, suppressScrollY: true});
+const psOoTabHst        = new PerfectScrollbar(OoTabHstScrllBody, {wheelSpeed: 0.50, wheelPropagation: true, minScrollbarLength: 10, suppressScrollX: true});
+/* Open Orders Tab Perfect Scrollbar */
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    if (OoTabOorScrllBody.querySelectorAll('.ps--active-y')){OoTabOorScrllHead.classList.add('active-y');}
+    if (OoTabHstScrllBody.querySelectorAll('.ps--active-y')){OoTabHstScrllHead.classList.add('active-y');}
+
+    OoTabHstScrllTble.addEventListener('ps-scroll-x', () =>{
+        OoTabHstScrllTble.style.width = OoTabHstArea.clientWidth + "px";
+    });
+
+    psOoTabOOr.update();
+    psOoTabHTb.update();
+    psOoTabHst.update();
+});
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const OoHtTblBrwsWrp = document.querySelectorAll('.openorders-tabhistory-table .scrollbar-tbl-tb .hsttblbrwswrp');
+    const OoHtTblBrwsBtn = document.querySelectorAll('.openorders-tabhistory-table .scrollbar-tbl-tb .hsttblbrwswrp .hsttblbrwstr');
+    const OoHtTblBrwsDiv = document.querySelectorAll('.openorders-tabhistory-table .scrollbar-tbl-tb .hsttblbrwswrp .hsttblbrwstbl');
+
+    for (let i = 0; i < OoHtTblBrwsWrp.length; i++) {
+
+        OoHtTblBrwsWrp.forEach(element => {
+            OoHtTblBrwsDiv[i].style.maxWidth = (OoTabHstArea.clientWidth - 10) + "px";
+            OoHtTblBrwsBtn[i].onclick = (e) => {
+                OoHtTblBrwsBtn[i].classList.toggle("active");
+                OoHtTblBrwsDiv[i].classList.toggle("show");
+            };
+        });
+
+    }
+});
