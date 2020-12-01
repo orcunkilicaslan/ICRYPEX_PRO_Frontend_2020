59b21ee04cd55af2914cc88299525fa776ea8ad5
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-12-01 08:18:41 +0300

    Component Set Alart Modal Finished

--- assets/src/script/custom/comp_modals.js
+++ assets/src/script/custom/comp_modals.js
@@ -3,5 +3,5 @@ import Modal from "bootstrap.native/dist/components/modal-native";
 
 /* Modal */
 //var myNewModalOpenOrdersDelete = new Modal('#ooModalDeleteOrder', { backdrop: true }).show();
-//var myNewModalSetAlarm = new Modal('#modalSetAlarm', { backdrop: true }).show();
+var myNewModalSetAlarm = new Modal('#modalSetAlarm', { backdrop: true }).show();
 /* Modal */
