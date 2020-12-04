e7524df466ea9e85918d75fe277f5124e576f2cb
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-12-04 11:15:35 +0300

    Component User Menu Modal Finished

--- assets/src/script/custom/modal_modals.js
+++ assets/src/script/custom/modal_modals.js
@@ -7,5 +7,6 @@ import Modal from "bootstrap.native/dist/components/modal-native";
 //var myNewModalNotifications = new Modal('#modalHeadNotifications', { backdrop: true }).show();
 //var myNewModalSignUp = new Modal('#modalHeadSignUp', { backdrop: true }).show();
 //var myNewModalSignIn = new Modal('#modalHeadSignIn', { backdrop: true }).show();
-var myNewModalNewsAll = new Modal('#modalHeadNewsAll', { backdrop: true }).show();
+//var myNewModalNewsAll = new Modal('#modalHeadNewsAll', { backdrop: true }).show();
+var myNewModalUserMenu = new Modal('#modalHeadUserMenu', { backdrop: true }).show();
 /* Modal */
