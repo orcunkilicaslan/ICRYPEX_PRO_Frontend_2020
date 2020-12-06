795a14ccc3b986c7e1adf41e3aa7393bc70af4eb
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-12-06 08:14:35 +0300

    Copy Text to Clipboard Added and General Fixed

---
+++ assets/src/script/custom/alert_toasts.js
@@ -0,0 +1,6 @@
+
+import Toast from "bootstrap.native/dist/components/toast-native";
+
+/* Toast */
+//var myNewToastCopyToClipboard = new Toast('#toastCopyToClipboard', { placement: 'bottom', autohide: false }).show();
+/* Toast */
