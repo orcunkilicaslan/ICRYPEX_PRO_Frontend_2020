21ccde932f25c7419c7dfb881d19b4e417179402
Doruk Kutlu <me@doruk.me>
2020-12-23 12:21:34 +0300

    render current year dynamically

--- react-app/src/components/Footer/FooterLeft.jsx
+++ react-app/src/components/Footer/FooterLeft.jsx
@@ -2,7 +2,7 @@
 const FooterLeft = props => {
   return (
       <div className="col">
-        All Rights Reserved © 2018-2019 Icrypex Crypto Exchange
+        All Rights Reserved © 2018-{new Date().getFullYear()} Icrypex Crypto Exchange
       </div>
   );
 };
