2c0df05a166e53633dba53f9ea096506326c8318
Doruk Kutlu <me@doruk.me>
2021-01-01 17:01:51 +0300

    <Table> | MarketDataSymbol refactor

--- react-app/src/components/MarketData/MarketDataSymbol.jsx
+++ react-app/src/components/MarketData/MarketDataSymbol.jsx
@@ -1,144 +1,137 @@
+import { ButtonGroup, Input } from "reactstrap";
+
 import { ReactComponent as MdTableFavIcon } from "../../assets/images/icons/path_icon_mdtable_fav.svg";
 import { ReactComponent as MdTableSearchIcon } from "../../assets/images/icons/path_icon_mdtable_search.svg";
 import { ReactComponent as PerLineIcon } from "../../assets/images/icons/path_icon_pericon.svg";
-
-import { ButtonGroup, Input} from 'reactstrap';
 import { Button } from "../Button";
-import { TableMain, TableThead, TableTbody, TableTr, TableTh, TableTd } from "../TableMain.jsx";
+import Table from "../Table.jsx";
 
 const mdsymboltable = [
-
-    {
-        pair: "BTC/TRY",
-        data: {
-            buy     : "3.63500",
-            sell    : "2.45300",
-            volume  : "10.23M",
-            change  : "-2.22%",
-            mdper   : "up",
-        }
+  {
+    pair: "BTC/TRY",
+    data: {
+      buy: "3.63500",
+      sell: "2.45300",
+      volume: "10.23M",
+      change: "-2.22%",
+      mdper: "up",
     },
-    {
-        pair: "BTC/USD",
-        data: {
-            buy     : "5.88721",
-            sell    : "1.095200",
-            volume  : "14.02M",
-            change  : "+1.38%",
-            mdper   : "down",
-        }
+  },
+  {
+    pair: "BTC/USD",
+    data: {
+      buy: "5.88721",
+      sell: "1.095200",
+      volume: "14.02M",
+      change: "+1.38%",
+      mdper: "down",
     },
-    {
-        pair: "BTC/USDT",
-        data: {
-            buy     : "4.99102",
-            sell    : "4.05740",
-            volume  : "24.80M",
-            change  : "+2.24%",
-            mdper   : "down",
-        }
+  },
+  {
+    pair: "BTC/USDT",
+    data: {
+      buy: "4.99102",
+      sell: "4.05740",
+      volume: "24.80M",
+      change: "+2.24%",
+      mdper: "down",
     },
-    {
-        pair: "ETH/TRY",
-        data: {
-            buy     : "1.88932",
-            sell    : "3.94618",
-            volume  : "12.39M",
-            change  : "+3.57%",
-            mdper   : "up",
-        }
-    }
-
+  },
+  {
+    pair: "ETH/TRY",
+    data: {
+      buy: "1.88932",
+      sell: "3.94618",
+      volume: "12.39M",
+      change: "+3.57%",
+      mdper: "up",
+    },
+  },
 ];
 
 const MarketDataSymbol = props => {
-    return (
-        <div className="marketdata-symbol">
-            <div className="tabcont tabcont-filterbar siteformui row">
-                <ButtonGroup size="sm" className="col">
-                    <Button
-                        type="button"
-                        size="sm"
-                        variant="secondary active"
-                    >
-                        <MdTableFavIcon className="filterfavico" />
-                    </Button>
-                    <Button
-                        type="button"
-                        size="sm"
-                        variant="secondary"
-                    >
-                        TRY
-                    </Button>
-                    <Button
-                        type="button"
-                        size="sm"
-                        variant="secondary"
-                    >
-                        USD
-                    </Button>
-                    <Button
-                        type="button"
-                        size="sm"
-                        variant="secondary"
-                    >
-                        USDT
-                    </Button>
-                    <Button
-                        type="button"
-                        size="sm"
-                        variant="secondary"
-                    >
-                        Tümü
-                    </Button>
-                </ButtonGroup>
-                <ButtonGroup size="sm" className="col-auto">
-                    <Input className="mdsearchinput" bsSize="sm" placeholder="Arama" />
-                    <div className="mdsearchicon">
-                        <MdTableSearchIcon />
-                    </div>
-                </ButtonGroup>
-            </div>
-            <div className="mdsymboltable scrollbar">
-                <TableMain scrollbar>
-                    <TableThead scrollbar>
-                        <TableTr>
-                            <TableTh sizeauto className="fav"></TableTh>
-                            <TableTh sizefixed className="sym">Sembol</TableTh>
-                            <TableTh sizefixed className="buy">Alış</TableTh>
-                            <TableTh sizefixed className="sll">Satış</TableTh>
-                            <TableTh sizefixed className="vol">Hacim</TableTh>
-                            <TableTh sizefixed className="chg">Değişim</TableTh>
-                            <TableTh sizeauto className="per"></TableTh>
-                        </TableTr>
-                    </TableThead>
-                    <TableTbody scrollbar striped hovered>
-
-                        {mdsymboltable.map(({ pair, data:{buy, sell, volume, change, mdper} }) => {
-                            return (
-                                <TableTr key={pair}>
-                                    <TableTd sizeauto className="fav">
-                                        <a className="tablefavico" href="#" title="Favorime Ekle">
-                                            <MdTableFavIcon />
-                                        </a>
-                                    </TableTd>
-                                    <TableTd sizefixed className="sym">{pair}</TableTd>
-                                    <TableTd sizefixed className="buy">{buy}</TableTd>
-                                    <TableTd sizefixed className="sll">{sell}</TableTd>
-                                    <TableTd sizefixed className="vol">{volume}</TableTd>
-                                    <TableTd sizefixed className="chg">{change}</TableTd>
-                                    <TableTd sizeauto className="per">
-                                        <PerLineIcon className={"mdper mdper-" + mdper} />
-                                    </TableTd>
-                                </TableTr>
-                            );
-                        })}
-
-                    </TableTbody>
-                </TableMain>
-            </div>
-        </div>
-    );
+  return (
+    <div className="marketdata-symbol">
+      <div className="tabcont tabcont-filterbar siteformui row">
+        <ButtonGroup size="sm" className="col">
+          <Button type="button" size="sm" variant="secondary active">
+            <MdTableFavIcon className="filterfavico" />
+          </Button>
+          {["TRY", "USD", "USDT", "Tümü"].map(elem => {
+            return (
+              <Button type="button" size="sm" variant="secondary">
+                {elem}
+              </Button>
+            );
+          })}
+        </ButtonGroup>
+        <ButtonGroup size="sm" className="col-auto">
+          <Input className="mdsearchinput" bsSize="sm" placeholder="Arama" />
+          <div className="mdsearchicon">
+            <MdTableSearchIcon />
+          </div>
+        </ButtonGroup>
+      </div>
+      <div className="mdsymboltable scrollbar">
+        <Table scrollbar>
+          <Table.Thead scrollbar>
+            <Table.Tr>
+              <Table.Th sizeauto className="fav"></Table.Th>
+              <Table.Th sizefixed className="sym">
+                Sembol
+              </Table.Th>
+              <Table.Th sizefixed className="buy">
+                Alış
+              </Table.Th>
+              <Table.Th sizefixed className="sll">
+                Satış
+              </Table.Th>
+              <Table.Th sizefixed className="vol">
+                Hacim
+              </Table.Th>
+              <Table.Th sizefixed className="chg">
+                Değişim
+              </Table.Th>
+              <Table.Th sizeauto className="per"></Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody scrollbar striped hovered>
+            {mdsymboltable.map(
+              ({ pair, data: { buy, sell, volume, change, mdper } }) => {
+                return (
+                  <Table.Tr key={pair}>
+                    <Table.Td sizeauto className="fav">
+                      <a className="tablefavico" href="#" title="Favorime Ekle">
+                        <MdTableFavIcon />
+                      </a>
+                    </Table.Td>
+                    <Table.Td sizefixed className="sym">
+                      {pair}
+                    </Table.Td>
+                    <Table.Td sizefixed className="buy">
+                      {buy}
+                    </Table.Td>
+                    <Table.Td sizefixed className="sll">
+                      {sell}
+                    </Table.Td>
+                    <Table.Td sizefixed className="vol">
+                      {volume}
+                    </Table.Td>
+                    <Table.Td sizefixed className="chg">
+                      {change}
+                    </Table.Td>
+                    <Table.Td sizeauto className="per">
+                      <PerLineIcon className={`mdper mdper-${mdper}`} />
+                    </Table.Td>
+                  </Table.Tr>
+                );
+              }
+            )}
+          </Table.Tbody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default MarketDataSymbol;
---
+++ react-app/src/components/Table.jsx
@@ -0,0 +1,110 @@
+import classNames from "classnames";
+
+const Table = props => {
+  const { className, scrollbar, children, ...rest } = props;
+
+  const tableClass = classNames("sitetablediv", className, {
+    "scrollbar-tbl": Boolean(scrollbar),
+  });
+
+  return (
+    <div className={tableClass} {...rest}>
+      {children}
+    </div>
+  );
+};
+
+Table.Thead = Thead;
+Table.Tbody = Tbody;
+Table.Tr = Tr;
+Table.Th = Th;
+Table.Td = Td;
+
+export default Table;
+
+export function Thead(props) {
+  const { className, scrollbar, children, ...rest } = props;
+  const tableClass = classNames("tbl-thead", className);
+
+  const tableThead = (
+    <div className={tableClass} {...rest}>
+      {children}
+    </div>
+  );
+
+  if (scrollbar) {
+    const scrollbarClassName = classNames({
+      "scrollbar-tbl-th": Boolean(scrollbar),
+    });
+
+    return <div className={scrollbarClassName}>{tableThead}</div>;
+  }
+
+  return tableThead;
+}
+
+export function Tbody(props) {
+  const { className, scrollbar, striped, hovered, children, ...rest } = props;
+
+  const tableClass = classNames("tbl-tbody", className, {
+    "tbl-striped": Boolean(striped),
+    "tbl-hovered": Boolean(hovered),
+  });
+
+  const tableTbody = (
+    <div className={tableClass} {...rest}>
+      {children}
+    </div>
+  );
+
+  if (scrollbar) {
+    const scrollbarClassName = classNames({
+      "scrollbar-tbl-tb": Boolean(scrollbar),
+    });
+
+    return <div className={scrollbarClassName}>{tableTbody}</div>;
+  }
+
+  return tableTbody;
+}
+
+export function Tr(props) {
+  const { className, children, ...rest } = props;
+  const tableTrClass = classNames("tbl-tr", className);
+
+  return (
+    <div className={tableTrClass} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+export function Th(props) {
+  const { className, sizefixed, sizeauto, children, ...rest } = props;
+
+  const tableThClass = classNames("tbl-th", className, {
+    fxd: Boolean(sizefixed),
+    aut: Boolean(sizeauto),
+  });
+
+  return (
+    <div className={tableThClass} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+export function Td(props) {
+  const { className, sizefixed, sizeauto, children, ...rest } = props;
+
+  const tableTdClass = classNames("tbl-td", className, {
+    fxd: Boolean(sizefixed),
+    aut: Boolean(sizeauto),
+  });
+
+  return (
+    <div className={tableTdClass} {...rest}>
+      {children}
+    </div>
+  );
+}
