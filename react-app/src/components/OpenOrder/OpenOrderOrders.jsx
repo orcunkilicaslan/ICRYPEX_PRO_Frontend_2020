dd9aaecc2be564d3b2b715d67d54f5ca248f1634
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-01-04 07:36:30 +0300

    Revert "Transaction History Added"
    This reverts commit 2d9b98ff7f71d08f9662a75c7a7b7aa65f5b1f66.

--- react-app/src/components/OpenOrder/OpenOrderOrders.jsx
+++ react-app/src/components/OpenOrder/OpenOrderOrders.jsx
@@ -74,17 +74,17 @@ const OpenOrderOrders = props => {
             <Row className="tabcont tabcont-filterbar siteformui">
                 <Col xs="auto">
                     <Input className="custom-select custom-select-sm" type="select">
-                        {["Çift", "...", "..."].map((el, idx) => {
-                            return <option key={`${el}_${idx}`}>{el}</option>
-                        })}
+                        <option>Çift</option>
+                        <option>...</option>
+                        <option>...</option>
                     </Input>
                 </Col>
                 <Col xs="auto">
                     <Input className="custom-select custom-select-sm" type="select">
                         <option selected disabled>İşlem Tipi</option>
-                        {["Stop Limit", "Market", "Limit"].map((el, idx) => {
-                            return <option key={`${el}_${idx}`}>{el}</option>
-                        })}
+                        <option>Stop Limit</option>
+                        <option>Market</option>
+                        <option>Limit</option>
                     </Input>
                 </Col>
                 <Col>
@@ -130,7 +130,7 @@ const OpenOrderOrders = props => {
                 </Col>
             </Row>
             <div className="ooopenorderstable scrollbar">
-                <Table scrollbar>
+                <Table>
                     <Table.Thead scrollbar>
                         <Table.Tr>
                             <Table.Th sizeauto className="symb">Çift</Table.Th>
