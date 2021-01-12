b1955903486c626cc199a427e15ace7321f562f6
Doruk Kutlu <me@doruk.me>
2021-01-12 21:47:25 +0300

    update craco devServer config

--- react-app/.cracorc.js
+++ react-app/.cracorc.js
@@ -12,17 +12,6 @@ const {
 const { SUPPORTED_LANGUAGES } = require("./src/setupI18n");
 
 module.exports = {
-  // babel: {
-  //   plugins: [
-  //     [
-  //       "i18next-extract",
-  //       {
-  //         locales: SUPPORTED_LANGUAGES,
-  //         outputPath: "translations/{{locale}}/{{ns}}.json",
-  //       },
-  //     ],
-  //   ],
-  // },
   webpack: {
     alias: {
       "~": path.resolve(__dirname, "src"),
@@ -35,7 +24,21 @@ module.exports = {
       },
     },
   },
-  // devServer: {
+  // babel: {
+  //   plugins: [
+  //     [
+  //       "i18next-extract",
+  //       {
+  //         locales: SUPPORTED_LANGUAGES,
+  //         outputPath: "translations/{{locale}}/{{ns}}.json",
+  //       },
+  //     ],
+  //   ],
+  // },
+  // devServer: whenDev(() => ({
+  //   https: true,
+  //   pfx: fs.readFileSync(path.resolve("./localhost.pfx")),
+  //   pfxPassphrase: "temp123!",
   //   proxy: {
   //     "/api": {
   //       target: "https://mobile.guvenlibtc.com:443/api",
@@ -43,5 +46,5 @@ module.exports = {
   //       secure: false,
   //     },
   //   },
-  // },
+  // })),
 };
