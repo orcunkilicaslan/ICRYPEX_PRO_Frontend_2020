4eada32d95a1804882e40363995e31412d9ff604
Doruk Kutlu <me@doruk.me>
2021-01-15 02:00:48 +0300

    fetch api tokens before rendering <App>

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,37 +1,12 @@
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 
 import "./App.scss";
 import Header from "./components/Header/Header.jsx";
 import { Main } from "./components/Main/Main.jsx";
 // import { Footer } from "./components/Footer/Footer";
-import {
-  fetchPreloginToken,
-  fetchServerDeviceKey,
-  setDeviceId,
-  setLocalKey,
-} from "~/state/slices/api.slice";
-import { makeLocalKey } from "~/util/";
-
-const MD5_secret = process.env.REACT_APP_MD5_SECRET;
 
 const App = props => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-  const init = async () => {
-    const { localkey, deviceuuid } = await makeLocalKey(MD5_secret);
-      dispatch(setDeviceId(deviceuuid));
-      dispatch(setLocalKey(localkey));
-      dispatch(fetchServerDeviceKey());
-      dispatch(fetchPreloginToken());
-  };
-
-    init();
-  }, []);
-
   return (
     <Router>
       <Header />
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -7,11 +7,21 @@ import "./index.scss";
 // import reportWebVitals from "./reportWebVitals";
 import { getStore } from "./state/";
 import initI18n from "./setupI18n";
+import { makeLocalKey } from "./util";
+import {
+  fetchPreloginToken,
+  fetchServerDeviceKey,
+  setDeviceId,
+  setLocalKey,
+} from "./state/slices/api.slice";
+
+const MD5_secret = process.env.REACT_APP_MD5_SECRET;
 
 run();
 
 let Store, Persistor;
 async function run() {
+  const { localkey, deviceuuid } = await makeLocalKey(MD5_secret);
   const { store, persistor } = await getStore();
   const {
     ui: { lang },
@@ -20,6 +30,10 @@ async function run() {
   Persistor = persistor;
 
   await initI18n(lang);
+  store.dispatch(setDeviceId(deviceuuid));
+  store.dispatch(setLocalKey(localkey));
+  await store.dispatch(fetchServerDeviceKey());
+  await store.dispatch(fetchPreloginToken());
   render();
 
   // Learn more: https://bit.ly/CRA-vitals
