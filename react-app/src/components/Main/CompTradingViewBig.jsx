2e8672dc13bbeae1ee3b2c0399eea0f4f90b3ccc
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-01-18 07:14:00 +0300

    TradingViewWidget Added

--- react-app/src/components/Main/CompTradingViewBig.jsx
+++ react-app/src/components/Main/CompTradingViewBig.jsx
@@ -1,8 +1,27 @@
+import TradingViewWidget from 'react-tradingview-widget';
+
 const CompTradingViewBig = props => {
   return (
     <div className="mainbox mainbox-tradingviewbig">
       <div className="tradingviewbig">
-        <div className="tradingview-widget-container"></div>
+        <div className="tradingview-widget-container">
+            <TradingViewWidget
+                symbol={"BINANCE:BTCTRY"}
+                theme={"Dark"}
+                locale={"TR"}
+                timezone={"Europe/Istanbul"}
+                interval={"5"}
+                style={"1"}
+                toolbar_bg={"#f1f3f6"}
+                hide_legend={true}
+                hide_side_toolbar={false}
+                hide_top_toolbar={false}
+                allow_symbol_change={false}
+                withdateranges={true}
+                save_image={false}
+                autosize
+            />
+        </div>
       </div>
     </div>
   );
