450f6b91b09f1f1945d5277a490676ab777879dd
Doruk Kutlu <me@doruk.me>
2021-01-26 21:27:17 +0300

    use whatwg-fetch for XHR

--- react-app/src/state/api/index.js
+++ react-app/src/state/api/index.js
@@ -1,82 +1,88 @@
-import { create } from "apisauce";
-import retry, { exponentialDelay } from "axios-retry";
+// import { create } from "apisauce";
+// import retry, { exponentialDelay } from "axios-retry";
+import "yet-another-abortcontroller-polyfill";
+import "event-target-polyfill";
+import { fetch as _fetch } from "whatwg-fetch";
+import { nanoid } from "nanoid";
+import merge from "lodash/merge";
 
 import { store } from "..";
 
 const isProd = process.env.NODE_ENV === "production";
 const API_BASE = process.env.REACT_APP_API_BASE;
-const instance = create({
-  baseURL: isProd ? API_BASE : null,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+const baseURL = isProd ? API_BASE : "";
+const fetch = "signal" in new Request("") ? window.fetch : _fetch;
+const AbortControllers = new Map();
+
+const instance = {
+  post: (uri, body, { headers }) => {
+    const { ui } = store.getState();
+    const controller = new AbortController();
+    const _id = nanoid();
+    const options = {
+      method: "POST",
+      body,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "x-culture-code": ui.lang || "tr",
+      },
+      credentials: "same-origin",
+      signal: controller.signal,
+    };
+
+    AbortControllers.set(_id, controller);
+    merge(options.headers, headers);
+
+    const request = fetch(`${baseURL}${uri}`, options)
+      .then(async response => {
+        const { ok, status, statusText, url } = response;
+        console.log(`Response: ${url} | ${status}`, response);
+
+        if (ok) {
+          const data = await response.json();
+          if (data?.status) {
+            return { data };
+          } else {
+            const error = new Error(data.errormessage);
+            error.name = "INTERNAL";
+            error.type = data.type;
+            error.data = data;
+            error.response = response;
+            throw error;
+          }
+        } else {
+          const error = new Error(statusText);
+          error.response = response;
+          throw error;
+        }
+      })
+      .catch(error => {
+        if (error.name === "AbortError") {
+          console.warn(`Request ${_id} was aborted`);
+        }
+
+        if (error.name === "INTERNAL") {
+          const { response, type, message } = error;
+          console.error(`${response.url} | ${type} | ${message}`);
+
+          switch (error.type) {
+            case "prelogintoken":
+            case "accesstoken":
+            default:
+              return Promise.reject(error);
+          }
+        }
+      });
+
+    request.id = _id;
+    return request;
   },
-});
-
-retry(instance.axiosInstance, {
-  retries: isProd ? 30 : 5,
-  retryCondition: error => {
-    return error.config?.method === "post";
+  abort: id => {
+    const controller = AbortControllers.get(id);
+    if (controller) controller?.abort();
   },
-  retryDelay: (count, error) => {
-    const url = error.config?.url;
-    const method = error.config?.method;
-    console.warn(`${method} | ${url} | retry no ${count}`);
-
-    return exponentialDelay(count);
-  },
-});
-
-instance.addAsyncRequestTransform(request => async () => {
-  const { method, url, headers } = request;
-  console.log("req: %s | %s | %O", method, url, request);
-  const { ui } = store.getState();
-
-  headers["x-culture-code"] = ui.lang || "tr";
-});
-
-instance.addAsyncResponseTransform(async response => {
-  const { ok, status, config, data, problem } = response;
-  console.log(
-    "res: %s | %s | %s %O",
-    config?.method,
-    config?.url,
-    status,
-    response
-  );
-
-  if (ok) {
-    if (data?.status) return response;
-
-    const type = data?.type;
-    const errormessage = data?.errormessage;
-    console.error(
-      "%s | %s | %s %s",
-      config?.method,
-      config?.url,
-      type,
-      errormessage
-    );
-
-    switch (type) {
-      case "prelogintoken":
-      case "accesstoken":
-      default:
-        return Promise.reject(response);
-    }
-  } else {
-    switch (problem) {
-      case "CLIENT_ERROR":
-      case "SERVER_ERROR":
-      case "TIMEOUT_ERROR":
-      case "CONNECTION_ERROR":
-      case "NETWORK_ERROR":
-      case "CANCEL_ERROR":
-      default:
-        return Promise.reject(response);
-    }
-  }
-});
+};
 
 export * from "./requests";
 export default instance;
