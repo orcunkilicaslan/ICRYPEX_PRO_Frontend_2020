f2e0bd8d0a6bb418cb7bf1a23583e554058f81d5
Doruk Kutlu <me@doruk.me>
2021-01-28 16:37:54 +0300

    refactor OpenOrderAccountActivitiesHistory

--- react-app/src/components/OpenOrder/OpenOrderAccountActivitiesHistory.jsx
+++ react-app/src/components/OpenOrder/OpenOrderAccountActivitiesHistory.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { Col, Input, Row } from "reactstrap";
+import classnames from "classnames";
+import { format } from "date-fns";
+
 import { Button } from "../Button.jsx";
 import Table from "../Table.jsx";
-import classnames from "classnames";
 
+const activityTypes = ["İşlem Tipi", "Stop Limit", "Market", "Limit"];
+const paymentMethods = ["Yöntem", "Havale-EFT", "Papara", "Kripto"];
+const activityStates = ["Durum", "Gerçekleşti", "Beklemede"];
+const activityCurrencies = ["Para Birimi", "TRY", "USD"];
 const historytable = [
   {
     transactionno: "MR-99999",
@@ -21,167 +27,199 @@ const historytable = [
 ];
 
 const OpenOrderAccountActivitiesHistory = props => {
-
-  const [selected1, setSelected1] = useState("");
-  const [selected2, setSelected2] = useState("");
-  const [selected3, setSelected3] = useState("");
-  const [selected4, setSelected4] = useState("");
+  const today = format(new Date(), "yyyy-MM-dd");
+  const [activityType, setActivityType] = useState(activityTypes[0]);
+  const [paymentMethod, setPaymentMethod] = useState(paymentMethods[0]);
+  const [activityState, setActivityState] = useState(activityStates[0]);
+  const [activityCurrency, setActivityCurrency] = useState(
+    activityCurrencies[0]
+  );
+  const [selectedDate, setSelectedDate] = useState(today);
 
   return (
-      <div className="activities-history">
-        <Row className="tabcont tabcont-filterbar siteformui">
-          <Col>
-            <Input
-                className="custom-select custom-select-sm"
-                type="select"
-                value={selected1}
-                onChange={({ target }) => {
-                  setSelected1(target.value);
-                }}
-            >
-              {["İşlem Tipi", "Stop Limit", "Market", "Limit"].map((el, idx) => {
-                return <option disabled={idx === 0} key={`${el}_${idx}`}>{el}</option>;
-              })}
-            </Input>
-          </Col>
-          <Col>
-            <Input
-                className="custom-select custom-select-sm"
-                type="select"
-                value={selected2}
-                onChange={({ target }) => {
-                  setSelected2(target.value);
-                }}
-            >
-              {["Yöntem", "Havale-EFT", "Papara", "Kripto"].map((el, idx) => {
-                return <option disabled={idx === 0} key={`${el}_${idx}`}>{el}</option>;
-              })}
-            </Input>
-          </Col>
-          <Col>
-            <Input
-                className="custom-select custom-select-sm"
-                type="select"
-                value={selected3}
-                onChange={({ target }) => {
-                  setSelected3(target.value);
-                }}
-            >
-              {["Durum", "Gerçekleşti", "Beklemede"].map((el, idx) => {
-                return <option selected={idx === 0} disabled={idx === 0} key={`${el}_${idx}`}>{el}</option>;
-              })}
-            </Input>
-          </Col>
-          <Col>
-            <Input
-                className="custom-select custom-select-sm"
-                type="select"
-                value={selected4}
-                onChange={({ target }) => {
-                  setSelected4(target.value);
-                }}
-            >
-              {["Para Birimi", "TRY", "USD"].map((el, idx) => {
+    <div className="activities-history">
+      <Row className="tabcont tabcont-filterbar siteformui">
+        <Col>
+          <Input
+            className="custom-select custom-select-sm"
+            type="select"
+            value={activityType}
+            onChange={({ target }) => {
+              setActivityType(target.value);
+            }}
+          >
+            {activityTypes.map((el, idx) => {
+              return (
+                <option disabled={idx === 0} key={`${el}_${idx}`}>
+                  {el}
+                </option>
+              );
+            })}
+          </Input>
+        </Col>
+        <Col>
+          <Input
+            className="custom-select custom-select-sm"
+            type="select"
+            value={paymentMethod}
+            onChange={({ target }) => {
+              setPaymentMethod(target.value);
+            }}
+          >
+            {paymentMethods.map((el, idx) => {
+              return (
+                <option disabled={idx === 0} key={`${el}_${idx}`}>
+                  {el}
+                </option>
+              );
+            })}
+          </Input>
+        </Col>
+        <Col>
+          <Input
+            className="custom-select custom-select-sm"
+            type="select"
+            value={activityState}
+            onChange={({ target }) => {
+              setActivityState(target.value);
+            }}
+          >
+            {activityStates.map((el, idx) => {
+              return (
+                <option disabled={idx === 0} key={`${el}_${idx}`}>
+                  {el}
+                </option>
+              );
+            })}
+          </Input>
+        </Col>
+        <Col>
+          <Input
+            className="custom-select custom-select-sm"
+            type="select"
+            value={activityCurrency}
+            onChange={({ target }) => {
+              setActivityCurrency(target.value);
+            }}
+          >
+            {activityCurrencies.map((el, idx) => {
+              return (
+                <option disabled={idx === 0} key={`${el}_${idx}`}>
+                  {el}
+                </option>
+              );
+            })}
+          </Input>
+        </Col>
+        <Col xs="auto">
+          <Input
+            type="date"
+            bsSize="sm"
+            value={selectedDate}
+            onChange={({ target }) => setSelectedDate(target.value)}
+            placeholder="Başlangıç - Bitiş Tarihi"
+          />
+        </Col>
+        <Col xs="auto">
+          <Button type="button" size="sm" variant="outline-primary">
+            Filtrele
+          </Button>
+        </Col>
+        <Col xs="auto">
+          <Button type="button" size="sm" variant="outline-danger">
+            Sıfırla
+          </Button>
+        </Col>
+      </Row>
+      <div className="activitieshistorytable scrollbar">
+        <Table scrollbar>
+          <Table.Thead scrollbar>
+            <Table.Tr>
+              <Table.Th sizeauto className="nmbr">
+                İşlem No
+              </Table.Th>
+              <Table.Th sizeauto className="date">
+                Tarih
+              </Table.Th>
+              <Table.Th sizeauto className="type">
+                İşlem Tipi
+              </Table.Th>
+              <Table.Th sizeauto className="mthd">
+                Yöntem
+              </Table.Th>
+              <Table.Th sizeauto className="bank">
+                Banka
+              </Table.Th>
+              <Table.Th sizefixed className="amnt">
+                Miktar
+              </Table.Th>
+              <Table.Th sizeauto className="txid">
+                Referans Kodu / TX ID
+              </Table.Th>
+              <Table.Th sizeauto className="stts">
+                Durum
+              </Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody striped hovered scrollbar>
+            {historytable.map(
+              ({
+                transactionno,
+                transactiondate,
+                transactiontime,
+                transactiontypeid,
+                transactiontypetxt,
+                transactionmethod,
+                transactionbank,
+                transactionamount,
+                transactionstatusid,
+                transactiontxid,
+                transactionstatustxt,
+              }) => {
+                const typecls = classnames({
+                  sitecolorgreen: transactiontypeid === "1",
+                  sitecolorred: transactiontypeid === "2",
+                });
+
+                const statuscls = classnames({
+                  sitecolorgreen: transactionstatusid === "1",
+                  sitecolorred: transactionstatusid === "2",
+                });
+
                 return (
-                    <option disabled={idx === 0} key={`${el}_${idx}`}>
-                      {el}
-                    </option>
+                  <Table.Tr key={transactionno}>
+                    <Table.Td sizeauto className="nmbr">
+                      {transactionno}
+                    </Table.Td>
+                    <Table.Td sizeauto className="date">
+                      {transactiondate} - {transactiontime}
+                    </Table.Td>
+                    <Table.Td sizeauto className="type">
+                      <span className={typecls}>{transactiontypetxt}</span>
+                    </Table.Td>
+                    <Table.Td sizeauto className="mthd">
+                      {transactionmethod}
+                    </Table.Td>
+                    <Table.Td sizeauto className="bank">
+                      {transactionbank}
+                    </Table.Td>
+                    <Table.Td sizefixed className="amnt">
+                      {transactionamount}
+                    </Table.Td>
+                    <Table.Td sizeauto className="txid">
+                      {transactiontxid}
+                    </Table.Td>
+                    <Table.Td sizeauto className="stts">
+                      <span className={statuscls}>{transactionstatustxt}</span>
+                    </Table.Td>
+                  </Table.Tr>
                 );
-              })}
-            </Input>
-          </Col>
-          <Col xs="auto">
-            <Input
-                type="text"
-                bsSize="sm"
-                placeholder="Başlangıç - Bitiş Tarihi"
-            />
-          </Col>
-          <Col xs="auto">
-            <Button type="button" size="sm" variant="outline-primary">
-              Filitrele
-            </Button>
-          </Col>
-          <Col xs="auto">
-            <Button type="button" size="sm" variant="outline-danger">
-              Sıfırla
-            </Button>
-          </Col>
-        </Row>
-        <div className="activitieshistorytable scrollbar">
-          <Table scrollbar>
-            <Table.Thead scrollbar>
-              <Table.Tr>
-                <Table.Th sizeauto className="nmbr">İşlem No</Table.Th>
-                <Table.Th sizeauto className="date">Tarih</Table.Th>
-                <Table.Th sizeauto className="type">İşlem Tipi</Table.Th>
-                <Table.Th sizeauto className="mthd">Yöntem</Table.Th>
-                <Table.Th sizeauto className="bank">Banka</Table.Th>
-                <Table.Th sizefixed className="amnt">Miktar</Table.Th>
-                <Table.Th sizeauto className="txid">Referans Kodu / TX ID</Table.Th>
-                <Table.Th sizeauto className="stts">Durum</Table.Th>
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody striped hovered scrollbar>
-              {historytable.map(
-                  ({
-                     transactionno,
-                     transactiondate,
-                     transactiontime,
-                     transactiontypeid,
-                     transactiontypetxt,
-                     transactionmethod,
-                     transactionbank,
-                     transactionamount,
-                     transactionstatusid,
-                     transactiontxid,
-                     transactionstatustxt
-                   }) => {
-
-                    const typecls = classnames({
-                      sitecolorgreen: transactiontypeid === "1",
-                      sitecolorred: transactiontypeid === "2",
-                    });
-
-                    const statuscls = classnames({
-                      sitecolorgreen: transactionstatusid === "1",
-                      sitecolorred: transactionstatusid === "2",
-                    });
-
-                    return (
-                        <Table.Tr key={transactionno}>
-                          <Table.Td sizeauto className="nmbr">
-                            {transactionno}
-                          </Table.Td>
-                          <Table.Td sizeauto className="date">
-                            {transactiondate} - {transactiontime}
-                          </Table.Td>
-                          <Table.Td sizeauto className="type">
-                            <span className={typecls}>{transactiontypetxt}</span>
-                          </Table.Td>
-                          <Table.Td sizeauto className="mthd">
-                            {transactionmethod}
-                          </Table.Td>
-                          <Table.Td sizeauto className="bank">
-                            {transactionbank}
-                          </Table.Td>
-                          <Table.Td sizefixed className="amnt">
-                            {transactionamount}
-                          </Table.Td>
-                          <Table.Td sizeauto className="txid">
-                            {transactiontxid}
-                          </Table.Td>
-                          <Table.Td sizeauto className="stts">
-                            <span className={statuscls}>{transactionstatustxt}</span>
-                          </Table.Td>
-                        </Table.Tr>
-                    );
-                  }
-              )}
-            </Table.Tbody>
-          </Table>
-        </div>
+              }
+            )}
+          </Table.Tbody>
+        </Table>
       </div>
+    </div>
   );
 };
 
