197c5895e3474a113dcf5ac3bd1871773bce6765
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-02-01 11:50:39 +0300

    Header Right Icons Access Token

--- react-app/src/components/Header/HeaderRightIcons.jsx
+++ react-app/src/components/Header/HeaderRightIcons.jsx
@@ -1,31 +1,39 @@
+import { useSelector } from "react-redux";
 import { UncontrolledTooltip } from "reactstrap";
 import { Button } from "../Button.jsx";
 import { IconSet } from "../IconSet.jsx";
 
 const HeaderRightIcons = props => {
-  return (
-      <div className="header-right-icons">
-        <Button
-            className="headsignedinicon fullscreen"
-            data-toggle="fullscreenbtn"
-        >
-          <IconSet sprite="sprtsmclrd" size="20" name="pagefullscreen" />
-        </Button>
-        <Button className="headsignedinicon support">
-          <IconSet sprite="sprtsmclrd" size="20" name="support" />
-        </Button>
-        <Button className="headsignedinicon notif">
-          <span id="headTooltipNotif">
-            <IconSet sprite="sprtsmclrd" size="20" name="notif">
-              <span className="badge badge-pill badge-danger">4</span>
-            </IconSet>
-          </span>
-          <UncontrolledTooltip placement="bottom" target="headTooltipNotif">
-            4 Yeni Bildiriminiz Var
-          </UncontrolledTooltip>
-        </Button>
-      </div>
-  );
+
+    const { accesstoken } = useSelector(state => state.api);
+
+    return (
+        <div className="header-right-icons">
+            <Button
+                className="headsignedinicon fullscreen"
+                data-toggle="fullscreenbtn"
+            >
+                <IconSet sprite="sprtsmclrd" size="20" name="pagefullscreen" />
+            </Button>
+            {accesstoken ? (
+                <Button className="headsignedinicon support">
+                    <IconSet sprite="sprtsmclrd" size="20" name="support" />
+                </Button>
+            ) : null}
+            {accesstoken ? (
+                <Button className="headsignedinicon notif">
+                    <span id="headTooltipNotif">
+                        <IconSet sprite="sprtsmclrd" size="20" name="notif">
+                            <span className="badge badge-pill badge-danger">4</span>
+                        </IconSet>
+                    </span>
+                    <UncontrolledTooltip placement="bottom" target="headTooltipNotif">
+                        4 Yeni Bildiriminiz Var
+                    </UncontrolledTooltip>
+                </Button>
+            ) : null}
+        </div>
+    );
 };
 
 export default HeaderRightIcons;
