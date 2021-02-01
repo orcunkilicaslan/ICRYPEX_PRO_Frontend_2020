07b2c17a39b7988dacf0fec01ca87c48fdd4f1c1
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-02-01 13:30:08 +0300

    Buy Sell Action Access Token

--- react-app/src/components/BuySellAction/BuySellActionStopLimit.jsx
+++ react-app/src/components/BuySellAction/BuySellActionStopLimit.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { useState } from "react";
 import {
   Row,
@@ -16,6 +17,8 @@ import { IconSet } from "../IconSet.jsx";
 import { Button } from "../Button.jsx";
 
 const BuySellActionStopLimit = props => {
+
+  const { accesstoken } = useSelector(state => state.api);
 
   const [selectedBuyPrice, setSelectedBuyPrice] = useState("");
   const [selectedBuyLimit, setSelectedBuyLimit] = useState("");
@@ -39,7 +42,7 @@ const BuySellActionStopLimit = props => {
               <h4 className="formhead-title">BTC AL</h4>
               <div className="formhead-curr">
                 <IconSet sprite="sprtsmclrd" size="16" name="wallet" />
-                <p>49,950,000.00 TRY</p>
+                <p>{accesstoken ? ("49,950,000.00") : ("-") } TRY</p>
               </div>
             </div>
             <div className="formfieldset">
@@ -111,50 +114,60 @@ const BuySellActionStopLimit = props => {
               </FormGroup>
             </div>
             <div className="formrange">
-              <Row className="aligncenter">
-                <Col xs="auto">
-                  <Label>Portföy</Label>
-                </Col>
-                <Col>
-                  <div className="rangeprogress">
-                    <Progress
-                        className="rangeprogress-progress"
-                        value={rangeBuyPortfolio}
-                    />
-                  </div>
-                  <div className="rangeprogress-circle d-none" data-val={rangeBuyPortfolio}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </div>
-                  <output className="rangeprogress-bubble"></output>
-                  <Input
-                      className="rangeprogress-range custom-range"
-                      type="range"
-                      min={100 / 4}
-                      max={100}
-                      step={100 / 4}
-                      value={rangeBuyPortfolio}
-                      onChange={({ target }) => {
-                        setRangeBuyPortfolio(target.value);
-                      }}
-                  />
-                </Col>
-              </Row>
+              {accesstoken ? (
+                  <Row className="aligncenter">
+                    <Col xs="auto">
+                      <Label>Portföy</Label>
+                    </Col>
+                    <Col>
+                      <div className="rangeprogress">
+                        <Progress
+                            className="rangeprogress-progress"
+                            value={rangeBuyPortfolio}
+                        />
+                      </div>
+                      <div className="rangeprogress-circle d-none" data-val={rangeBuyPortfolio}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </div>
+                      <output className="rangeprogress-bubble"></output>
+                      <Input
+                          className="rangeprogress-range custom-range"
+                          type="range"
+                          min={100 / 4}
+                          max={100}
+                          step={100 / 4}
+                          value={rangeBuyPortfolio}
+                          onChange={({ target }) => {
+                            setRangeBuyPortfolio(target.value);
+                          }}
+                      />
+                    </Col>
+                  </Row>
+              ) : null }
               <Row className="aligncenter">
                 <Col xs="auto">
                   <Label>Toplam</Label>
                 </Col>
                 <Col className="text-right">
-                  <span>12,000,00.00 TRY</span>
+                  <span>{accesstoken ? ("12,000,00.00") : ("-") } TRY</span>
                 </Col>
               </Row>
             </div>
             <div className="formbttm">
-              <Button variant="success">
-                BTC AL
-              </Button>
+              <div className="formbttm">
+                {accesstoken ? (
+                    <Button variant="success">
+                      BTC AL
+                    </Button>
+                ) : (
+                    <Button variant="secondary" className="active">
+                      ÜYE OLUN <span className="sitecolortextsec">YADA</span> GİRİŞ YAPIN
+                    </Button>
+                )}
+              </div>
             </div>
           </Form>
         </Col>
@@ -240,50 +253,58 @@ const BuySellActionStopLimit = props => {
               </FormGroup>
             </div>
             <div className="formrange">
-              <Row className="aligncenter">
-                <Col xs="auto">
-                  <Label>Portföy</Label>
-                </Col>
-                <Col>
-                  <div className="rangeprogress">
-                    <Progress
-                        className="rangeprogress-progress"
-                        value={rangeSellPortfolio}
-                    />
-                  </div>
-                  <div className="rangeprogress-circle d-none" data-val={rangeSellPortfolio}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </div>
-                  <output className="rangeprogress-bubble"></output>
-                  <Input
-                      className="rangeprogress-range custom-range"
-                      type="range"
-                      min={100 / 4}
-                      max={100}
-                      step={100 / 4}
-                      value={rangeSellPortfolio}
-                      onChange={({ target }) => {
-                        setRangeSellPortfolio(target.value);
-                      }}
-                  />
-                </Col>
-              </Row>
+              {accesstoken ? (
+                  <Row className="aligncenter">
+                    <Col xs="auto">
+                      <Label>Portföy</Label>
+                    </Col>
+                    <Col>
+                      <div className="rangeprogress">
+                        <Progress
+                            className="rangeprogress-progress"
+                            value={rangeSellPortfolio}
+                        />
+                      </div>
+                      <div className="rangeprogress-circle d-none" data-val={rangeSellPortfolio}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </div>
+                      <output className="rangeprogress-bubble"></output>
+                      <Input
+                          className="rangeprogress-range custom-range"
+                          type="range"
+                          min={100 / 4}
+                          max={100}
+                          step={100 / 4}
+                          value={rangeSellPortfolio}
+                          onChange={({ target }) => {
+                            setRangeSellPortfolio(target.value);
+                          }}
+                      />
+                    </Col>
+                  </Row>
+              ) : null }
               <Row className="aligncenter">
                 <Col xs="auto">
                   <Label>Toplam</Label>
                 </Col>
                 <Col className="text-right">
-                  <span>12,000,00.00 TRY</span>
+                  <span>{accesstoken ? ("12,000,00.00") : ("-") } TRY</span>
                 </Col>
               </Row>
             </div>
             <div className="formbttm">
-              <Button variant="danger">
-                BTC SAT
-              </Button>
+              {accesstoken ? (
+                  <Button variant="danger">
+                    BTC SAT
+                  </Button>
+              ) : (
+                  <Button variant="secondary" className="active">
+                    ÜYE OLUN <span className="sitecolortextsec">YADA</span> GİRİŞ YAPIN
+                  </Button>
+              )}
             </div>
           </Form>
         </Col>
