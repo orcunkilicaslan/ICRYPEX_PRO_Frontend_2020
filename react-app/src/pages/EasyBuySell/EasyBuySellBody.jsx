504c9115d32f88eaae5248df62f28f4ea9973249
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-02-03 09:16:02 +0300

    EasyBuySellBody Created

--- react-app/src/pages/EasyBuySell/EasyBuySellBody.jsx
+++ react-app/src/pages/EasyBuySell/EasyBuySellBody.jsx
@@ -1,5 +1,6 @@
 import { Container, Row } from "reactstrap";
 import MarketData from "~/components/MarketData/MarketData.jsx";
+import MainCont from "~/components/MainCont.jsx";
 
 const EasyBuySellBody = props => {
   return (
@@ -10,7 +11,12 @@ const EasyBuySellBody = props => {
                       <MarketData />
                   </aside>
                   <section className="main-body-rightcont col">
-                      TEST
+                      <MainCont box>
+                          <MainCont.Title titleName="Kolay Al Sat" />
+                          <MainCont.Content contentName="easybuysell">
+                              Kolay Al Sat Content
+                          </MainCont.Content>
+                      </MainCont>
                   </section>
               </Row>
           </Container>
