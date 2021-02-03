5975bd419369f155a7c40c403967adf0601a3027
Doruk Kutlu <me@doruk.me>
2021-02-03 17:48:46 +0300

    <EasyBuySell> refactor

--- react-app/src/pages/EasyBuySell/EasyBuySell.jsx
+++ react-app/src/pages/EasyBuySell/EasyBuySell.jsx
@@ -1,16 +1,17 @@
 import classNames from "classnames";
+
 import SectionCoinBar from "~/pages/Sections/SectionCoinBar.jsx";
 import EasyBuySellBody from "./EasyBuySellBody.jsx";
 import { Footer } from "~/components/Footer/Footer.jsx";
 
 export const EasyBuySell = props => {
-  const { className } = props;
+  const { className, ...rest } = props;
 
   return (
-      <main className={classNames("main", className)}>
-          <SectionCoinBar />
-          <EasyBuySellBody />
-          <Footer />
-      </main>
+    <main className={classNames("main", className)} {...rest}>
+      <SectionCoinBar />
+      <EasyBuySellBody />
+      <Footer />
+    </main>
   );
 };
