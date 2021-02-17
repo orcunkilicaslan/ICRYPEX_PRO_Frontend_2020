7be3b1d1179045ecf9efee8079ed403bbb813e3b
Doruk Kutlu <me@doruk.me>
2021-02-17 23:36:22 +0300

    use socket.io v3

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -47,6 +47,7 @@ async function run() {
   await store.dispatch(fetchServerDeviceKey());
   await store.dispatch(fetchPreloginToken());
   await store.dispatch(fetchSettings());
+  store.dispatch({ type: "socket/open" });
   render();
 }
 
--- react-app/src/state/middleware/socket.js
+++ react-app/src/state/middleware/socket.js
@@ -3,7 +3,6 @@ import { merge } from "lodash";
 import ms from "ms";
 
 import { debug } from "~/util";
-import { refreshToken } from "../slices/api.slice";
 import {
   connected,
   disconnected,
@@ -12,7 +11,6 @@ import {
   setOrderHistory,
 } from "../slices/socket.slice";
 
-let isSocketAlive;
 const log = debug.extend("socket");
 const defaults = {
   timeout: ms("30s"),
@@ -55,77 +53,26 @@ export default function createSocketMiddleware(options) {
     });
 
     client.io.on("reconnect_failed", () => log("reconnection failed"));
-    client.io.on("ping", () => {
-      if (!isSocketAlive) dispatch(refreshToken());
-      isSocketAlive = false;
-      log("ping");
+    // client.io.on("ping", () => log("ping"));
+
+    client.onAny((key, data) => {
+      if (key.endsWith("orderbook")) {
+        log("%s: %d buytotal", key, data?.buytotal);
+        dispatch(setOrderBook(key, data));
+      } else if (key === "prices") {
+        log("%s: %s pairs", key, data.length);
+        dispatch(setPrices(data));
+      } else if (key.endsWith("orderhistory")) {
+        log("%s: %d orders", key, data?.length);
+        dispatch(setOrderHistory(key, data));
+      }
     });
 
     return next => action => {
-      const { type, payload } = action;
-      const predicate = [
-        "api/signinwithsms/fulfilled",
-        "api/refreshtoken/fulfilled",
-        "api/settings/fulfilled",
-        "persist/REHYDRATE",
-      ].includes(type);
-      let token;
-
-      if (predicate) {
-        if (action.key === "api") {
-          // persistor actions have a key field
-          token = payload?.accesstoken;
-        } else if (type === "api/settings/fulfilled") {
-          const { pair } = getState();
-          const eventKeys = getEventKeys(pair?.symbols);
-
-          for (const key of eventKeys) {
-            client.on(key, data => { // eslint-disable-line
-              isSocketAlive = true; // eslint-disable-line
-              if (key.endsWith("orderbook")) {
-                log("%s: %d buytotal", key, data?.buytotal);
-                dispatch(setOrderBook(key, data));
-              } else if (key === "prices") {
-                log("%s: %s pairs", key, data.length);
-                dispatch(setPrices(data));
-              } else if (key.endsWith("orderhistory")) {
-                log("%s: %d orders", key, data?.length);
-                dispatch(setOrderHistory(key, data));
-              }
-            });
-          }
-          // client.onAny((event, data) => {
-          //   if (event === "prices" || eventKeys.includes(event)) {
-          //     myLog(event, data);
-          //     dispatch(mergeData({ [event]: data }));
-          //   }
-          // });
-        } else {
-          token = payload?.description;
-        }
-
-        if (token) {
-          client.io.opts.query = { token };
-
-          if (client.disconnected) {
-            log("connecting to %s", client.io.uri);
-            client.connect();
-          }
-        }
-      }
+      if (action.type === "socket/open") client.open();
+      if (action.type === "socket/close") client.close();
 
       next(action);
     };
   };
-}
-
-function getEventKeys(symbols = []) {
-  return symbols
-    .map(sym => {
-      const symbol = sym.toLowerCase();
-
-      return [`${symbol}orderbook`, `${symbol}orderhistory`];
-    })
-    .flat()
-    .concat(["prices"]);
 }
