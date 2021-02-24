e9df752385c6db5f3423b1f109933e8c6b9b85c1
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-02-24 09:26:00 +0300

    Asset No Balance React Added

--- react-app/src/components/OpenOrder/OpenOrderAssetsChartList.jsx
+++ react-app/src/components/OpenOrder/OpenOrderAssetsChartList.jsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 
 import { fetchAssets } from "~/state/slices/assets.slice";
+import { IconSet } from "~/components/IconSet";
+import { Button } from "~/components/Button";
 
 const assetchartlistoptions = {
   donut: true,
@@ -15,7 +17,7 @@ const assetchartlistoptions = {
 };
 
 const assetchartlisttype = "Pie";
-const currencies = ["TRY", "USD", "BTC"];
+const currencies = ["TRY", "USD", "EUR"];
 
 const OpenOrderAssetsChartList = props => {
   const dispatch = useDispatch();
@@ -37,53 +39,84 @@ const OpenOrderAssetsChartList = props => {
   }, []);
 
   return (
+
     <div className="assets-chartlist">
-      <div className="assetchartarea">
-        <div className="assetchartarea-donut">
-          <ChartistGraph
-            className="asssetdonutchrt"
-            data={chartData}
-            options={assetchartlistoptions}
-            type={assetchartlisttype}
-          />
-          <div className="asssetdonutinfo">
-            <div className="asssetdonutinfo-box">
-              <h6>{t("total")}</h6>
-              <p>
-                {allAssets?.[`total_${selectedCurrency}`]?.toFixed?.(2)}{" "}
-                {selectedCurrency}
-              </p>
-              <p>0.0028200 BTC</p>
+      {!allAssets?.[`total_${selectedCurrency}`] ? (
+          <div className="noassetbalance">
+            <div className="noassetbalance-content">
+              <IconSet sprite="sprtlgclrd" size="50gray" name="close" />
+              <h4>Bakiyeniz Bulunmamaktadır.</h4>
+              <p>Görüntüleyebilmeniz için aşağıdaki para yatırma seçeneklerden birisini tercih edebilirsiniz.</p>
+              <div className="contbtn">
+                <Button
+                    size="sm"
+                    variant="secondary"
+                >
+                  Banka
+                </Button>
+                <Button
+                    size="sm"
+                    variant="secondary"
+                >
+                  Papara
+                </Button>
+                <Button
+                    size="sm"
+                    variant="secondary"
+                >
+                  Kripto
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="assetchartarea-list">
-          <ul className="asssetalllist">
-            {allAssets?.balances?.map?.(({ symbol, balance }) => {
-              return (
-                <li
-                  className={`asssetcurr-${symbol?.toLowerCase?.()}`}
-                  key={symbol}
-                >
-                  <div className="circle">
-                    <i />
-                  </div>
-                  <div className="info">
-                    <h6>{symbol}</h6>
-                    <p>
-                      {
-                        currencies.includes(symbol)
-                            ? balance.toFixed?.(2)
-                            : balance
-                      }
-                    </p>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
+      ) : (
+          <div className="assetchartarea">
+            <div className="assetchartarea-donut">
+              <ChartistGraph
+                  className="asssetdonutchrt"
+                  data={chartData}
+                  options={assetchartlistoptions}
+                  type={assetchartlisttype}
+              />
+              <div className="asssetdonutinfo">
+                <div className="asssetdonutinfo-box">
+                  <h6>{t("total")}</h6>
+                  <p>
+                    {allAssets?.[`total_${selectedCurrency}`]?.toFixed?.(2)}{" "}
+                    {selectedCurrency}
+                  </p>
+                  <p>0.0028200 BTC</p>
+                </div>
+              </div>
+            </div>
+            <div className="assetchartarea-list">
+              <ul className="asssetalllist">
+                {allAssets?.balances?.map?.(({ symbol, balance }) => {
+                  return (
+                      <li
+                          className={`asssetcurr-${symbol?.toLowerCase?.()}`}
+                          key={symbol}
+                      >
+                        <div className="circle">
+                          <i />
+                        </div>
+                        <div className="info">
+                          <h6>{symbol}</h6>
+                          <p>
+                            {
+                              currencies.includes(symbol)
+                                  ? balance.toFixed?.(2)
+                                  : balance
+                            }
+                          </p>
+                        </div>
+                      </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+      )}
     </div>
   );
 };
