43506edb152c865e73bdb4894ea1432b054cf922
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-02-26 21:14:16 +0300

    Notif Modal New Frontend Fixed

--- assets/src/script/custom/modal_modals.js
+++ assets/src/script/custom/modal_modals.js
@@ -4,7 +4,7 @@ import Modal from "bootstrap.native/dist/components/modal-native";
 /* Modal */
 //var myNewModalOpenOrdersDelete = new Modal('#ooModalDeleteOrder', { backdrop: true }).show();
 //var myNewModalSetAlarm = new Modal('#modalSetAlarm', { backdrop: true }).show();
-var myNewModalNotifications = new Modal('#modalHeadNotifications', { backdrop: true }).show();
+//var myNewModalNotifications = new Modal('#modalHeadNotifications', { backdrop: true }).show();
 //var myNewModalSignUp = new Modal('#modalHeadSignUp', { backdrop: true }).show();
 //var myNewModalSignIn = new Modal('#modalHeadSignIn', { backdrop: true }).show();
 //var myNewModalSignInVerifCode = new Modal('#modalHeadSignInVerifCode', { backdrop: true }).show();
