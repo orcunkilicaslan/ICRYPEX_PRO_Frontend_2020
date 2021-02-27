fa80868fd3a843105483b611df367b4fa17ba064
Doruk Kutlu <me@doruk.me>
2021-02-27 05:44:00 +0300

    typo

--- react-app/src/state/index.js
+++ react-app/src/state/index.js
@@ -20,7 +20,7 @@ const persistConfig = {
   key: "root",
   storage,
   debug: Boolean(process.env.REACT_APP_DEBUG),
-  blacklist: ["api", "ui", "socket"],
+  blacklist: ["api", "ui", "socket", "alarm"],
 };
 const socketIoOptions = {
   url: REACT_APP_SOCKET_BASE,
