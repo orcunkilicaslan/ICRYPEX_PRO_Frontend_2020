0464ddd910a4707810206e915b30d535cc571f1f
Doruk Kutlu <me@doruk.me>
2021-05-24 12:34:15 +0300

    toggleSettingsModal() | arrowIcon className

--- react-app/src/components/Header/HeaderRightSignedIn.jsx
+++ react-app/src/components/Header/HeaderRightSignedIn.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import classnames from "classnames";
 
 import { Button } from "../Button.jsx";
 import { IconSet } from "../IconSet.jsx";
@@ -11,13 +12,19 @@ const HeaderRightSignedIn = props => {
   const { user, onSignout } = props;
   const dispatch = useDispatch();
   const { openModal } = useSelector(state => state.ui);
-
-  const openSettingsModal = () => {
-    dispatch(setOpenModal("settings"));
-  };
+  const isModalOpen = openModal === "settings";
+  const arrowIconClass = classnames("siteiconsdropdown", {
+    downdirection: !isModalOpen,
+    updirection: isModalOpen,
+  });
 
   const clearOpenModals = () => {
     dispatch(setOpenModal("none"));
+  };
+
+  const toggleSettingsModal = () => {
+    if (isModalOpen) clearOpenModals();
+    else dispatch(setOpenModal("settings"));
   };
 
   useEffect(() => {
@@ -26,17 +33,17 @@ const HeaderRightSignedIn = props => {
 
   return (
     <div className="header-right-signedin pr-2">
-      <Button className="useraccountarea" onClick={openSettingsModal}>
+      <Button className="useraccountarea" onClick={toggleSettingsModal}>
         <div className="useraccountarea-avatar rounded-pill">
           <IconSet sprite="sprtlgclrd" size="50gray" name="user" />
         </div>
         <h3 className="useraccountarea-name">
           {user?.info?.displayname || null}
         </h3>
-        <i className="siteiconsdropdown downdirection"></i>
+        <i className={arrowIconClass}></i>
       </Button>
       <SettingsModal
-        isOpen={openModal === "settings"}
+        isOpen={isModalOpen}
         clearModals={clearOpenModals}
         User={user}
         onSignout={onSignout}
